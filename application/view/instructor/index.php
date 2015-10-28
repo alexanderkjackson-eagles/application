ed8aa1d97e19f4d862679a6f250fdb88693ac638
alexanderkjackson-eagles <alexanderkjackson@eagles.usm.edu>
2015-10-28 09:31:57 +0000

    Major changes

--- application/view/instructor/index.php
+++ application/view/instructor/index.php
@@ -15,51 +15,11 @@
 		if(isset($_GET['page'])){
 			$var = $_GET['page'];
 			include '/var/www/html/our-html/Instructor Forms/' . $var;
+			if ($var == 'studentData.html')
+				include '/var/www/html/application/view/instructor/student_list.php';
 		}
 		else
 			include '/var/www/html/our-html/Instructor Forms/instructor.html';
 	?>
-        <div>
-            <table class="overview-table">
-	    	<center><strong>Students</center></strong>
-                <thead>
-                <tr>
-                    <td>Id</td>
-                    <td>Avatar</td>
-                    <td>Username</td>
-                    <td>User's email</td>
-                    <td>Activated ?</td>
-		    <td>In class ?</td>
-                    <td>Link to user's profile</td>
-                    <td>Remove from class</td>
-                    <td>Submit</td>
-                </tr>
-                </thead>
-                <?php foreach ($this->users as $user) { ?>
-                    <tr class="<?= ($user->user_active == 0 ? 'inactive' : 'active'); ?>">
-                        <td><?= $user->user_id; ?></td>
-                        <td class="avatar">
-                            <?php if (isset($user->user_avatar_link)) { ?>
-                                <img src="<?= $user->user_avatar_link; ?>"/>
-                            <?php } ?>
-                        </td>
-                        <td><?= $user->user_name; ?></td>
-                        <td><?= $user->user_email; ?></td>
-                        <td><?= ($user->user_active == 0 ? 'No' : 'Yes'); ?></td>
-                        <td>
-                            <a href="<?= Config::get('URL') . 'profile/showProfile/' . $user->user_id; ?>">Profile</a>
-                        </td>
-                        <form action="<?= config::get("URL"); ?>instructor/actionAccountSettings" method="post">
-                            <td><input type="number" name="suspension" /></td>
-                            <td><input type="checkbox" name="softDelete" <?php if ($user->user_deleted) { ?> checked <?php } ?> /></td>
-                            <td>
-                                <input type="hidden" name="user_id" value="<?= $user->user_id; ?>" />
-                                <input type="submit" />
-                            </td>
-                        </form>
-                    </tr>
-                <?php } ?>
-            </table>
-        </div>
     </div>
 </div>
