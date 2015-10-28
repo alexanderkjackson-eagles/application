47015b1f37fc8b18e9df7a5738567d0179248c89
alexanderkjackson-eagles <alexanderkjackson@eagles.usm.edu>
2015-10-28 01:42:22 -0500

    none

--- application/view/admin/index.php
+++ application/view/admin/index.php
@@ -12,7 +12,7 @@
             This controller/action/view checks for an update and automatically installs it if found. Additionally, it provides the basic admin page to demonstrate use cases for administrators (researchers).
         </div>
 	<div class="box">
-	<form method="get" action="/admin/">
+	<form method="get" action="/adminaoe/">
 		<button type="submit" value="manageSessions">Manage sessions</button>
 	</form>
 	<?php 
